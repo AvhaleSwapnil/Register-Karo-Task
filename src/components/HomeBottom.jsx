4ebaf3dcd242a3ed4cb2bcec7da20f28b528536a
AvhaleSwapnil <swapnilavhale460@gmail.com>
2025-03-08 13:04:17 +0530

    Home bottom ui completed

--- src/components/HomeBottom.jsx
+++ src/components/HomeBottom.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import oracle from "../assets/oracle.png";
+import morpheus from "../assets/morpheus.png";
+import samsung from "../assets/samsung.png";
+import monday from "../assets/monday.png";
+import segment from "../assets/segment.png";
 const HomeBottom = () => {
   return (
-    <div className="h-[236px] text-center px-10">
-      <p className="text-xl lg:text-2xl text-[#272D37] mt-18">
+    <div className="h-[164px] mt-18 text-center px-20">
+      <p className="text-xl lg:text-2xl text-[#272D37]">
         Trusted By Over 100+ Startups and freelance business
       </p>
-      <div>
-        <img src={oracle} alt="" width={"138px"} />
+      <div className="mt-10 flex items-center justify-around">
+        <div>
+          <img src={oracle} alt="" width={"138px"} />
+        </div>
+        <div>
+          <img src={morpheus} alt="" width={"138px"} />
+        </div>
+        <div>
+          <img src={morpheus} alt="" width={"138px"} />
+        </div>
+        <div>
+          <img src={samsung} alt="" width={"138px"} />
+        </div>
+        <div>
+          <img src={monday} alt="" width={"138px"} />
+        </div>
+        <div>
+          <img src={segment} alt="" width={"138px"} />
+        </div>
       </div>
     </div>
   );
